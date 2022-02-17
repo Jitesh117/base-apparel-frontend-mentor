33ee9282672af07c9973ea4583a0404550f4f264
Jitesh117 <sahookumarjitesh@gmail.com>
2022-02-17 14:53:22 +0530

    completed the desktop version

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ let nonetext = document.getElementById('nonetext');
 var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 if(inputText.value.match(mailformat))
 {
-alert("Valid email address!");
 document.form1.text1.focus();
 none.style.display = 'none';
-none.style.display = 'none'
+nonetext.style.display = 'none';
+
 return true;
 }
 else
 {
-alert("You have entered an invalid email address!");
 document.form1.text1.focus();
 none.style.display= 'inherit';
 nonetext.style.display= 'inherit';
